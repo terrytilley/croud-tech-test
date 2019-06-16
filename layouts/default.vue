c70df31e6b7373e1591cb22a54656a053c01cefb
Terry Tilley <terry@terrytilley.com>
2019-06-16 15:11:50 +0100

    :tada: init commit :ring:

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      title: 'Croud'
+    }
+  }
+}
+</script>
